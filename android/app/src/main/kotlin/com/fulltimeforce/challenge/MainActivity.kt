4b075f8212ae8441128517b015a6323e498ebc31
Gonzalo Caira <gcairaaguilar@gmail.com>
2024-02-01 10:21:58 -0500

    init project

---
+++ android/app/src/main/kotlin/com/fulltimeforce/challenge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fulltimeforce.challenge.fulltimeforce_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:fulltimeforce_challenge/app/app.dart';
+import 'package:fulltimeforce_challenge/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
